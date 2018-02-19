89982e9cee957cf232e555058a98c19e7de2830b
Rick <50fpsocr@gmail.com>
2018-02-19 13:44:59 +0200

    Updated functionality for Blog status

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,9 +1,9 @@
 class Blog < ApplicationRecord
 	enum status: { draft: 0, published: 1 }
-	# Blog.status = draft/published 
 	# now when a new blog is created it has status: 'draft'
 	# and is updated with Blog.last.published! to get status: 'published'
 	# use Blog.published to only view records with status: 'published'
+	# check status with Blog.last.draft? Blog.last.published?
 	extend FriendlyId
 	friendly_id :title, use: :slugged
 	# will use Blog.title as slug url index
--- db/migrate/20180219100838_add_post_status_to_blogs.rb
+++ db/migrate/20180219100838_add_post_status_to_blogs.rb
@@ -3,3 +3,5 @@ class AddPostStatusToBlogs < ActiveRecord::Migration[5.1]
     add_column :blogs, :status, :integer, default: 0
   end
 end
+
+# rails g migration add_post_status_to_blogs status:integer
