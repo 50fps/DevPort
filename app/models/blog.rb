81cf952ae07d37c4b857a6c942202437e8689378
Rick <50fpsocr@gmail.com>
2018-02-19 12:26:13 +0200

    Updated status for enum

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,10 @@
 class Blog < ApplicationRecord
+	enum status: { draft: 0, published: 1 }
+	# Blog.status = draft/published 
+	# now when a new blog is created it has status: 'draft'
+	# and is updated with Blog.last.published! to get status: 'published'
+	# use Blog.published to only view records with status: 'published'
 	extend FriendlyId
 	friendly_id :title, use: :slugged
+	# will use Blog.title as slug url index
 end
